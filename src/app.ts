e251c99e6d7272fa8f3f8dd52b9884b55de21946
shohrux <seltashoh@gmail.com>
2025-08-11 12:31:49 +0500

    Refactor authentication and menu endpoints; update mock data structure

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from "@fastify/cors";
-import fastify, { type FastifyRequest } from "fastify";
+import fastify from "fastify";
 import mockAuth from "./mocks/auth.json";
 import mockMenu from "./mocks/menus.json";
 import mockReceipt from "./mocks/receipt.json";
@@ -8,30 +8,23 @@ app.register(cors, {
   // put your options here
 });
 
-app.post(
-  "/qarshicafe/hs/mzakaz/auth",
-  (
-    req: FastifyRequest<{
-      Body: {
-        login: string;
-        pwd: string;
-      };
-    }>,
-    res
-  ) => {
-    if (req.body.login !== "admin" || req.body.pwd !== "pass") {
-      return new Error("Unauthorized");
-    }
-
-    return mockAuth;
+app.post("/qarshicafe/hs/mzakaz/auth", (req, res) => {
+  const { login, pwd } = JSON.parse(req.body as string) as {
+    login: string;
+    pwd: string;
+  };
+  if (login !== "admin" || pwd !== "pass") {
+    return new Error("Unauthorized");
   }
-);
 
-app.get("/qarshicafe/hs/mzakaz/menu", (req, res) => {
+  return mockAuth;
+});
+
+app.post("/qarshicafe/hs/mzakaz/menu", (req, res) => {
   return mockMenu;
 });
 
-app.get("/qarshicafe/hs/mzakaz/:id", (req, res) => {
+app.post("/qarshicafe/hs/mzakaz/:id", (req, res) => {
   return mockReceipt;
 });
 
